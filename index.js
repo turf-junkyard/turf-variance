d02e059b00456288501299054d9911ce8272ffed
morganherlocker <morgan.herlocker@gmail.com>
2014-12-12 11:19:35 -0500

    add benchmarking

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var ss = require('simple-statistics')
-var inside = require('turf-inside')
+var ss = require('simple-statistics');
+var inside = require('turf-inside');
 
-module.exports = function(polyFC, ptFC, inField, outField, done){
+module.exports = function (polyFC, ptFC, inField, outField, done) {
   polyFC.features.forEach(function(poly){
     if(!poly.properties){
-      poly.properties = {}
+      poly.properties = {};
     }
-    var values = []
+    var values = [];
     ptFC.features.forEach(function(pt){
       if (inside(pt, poly)) {
         values.push(pt.properties[inField]);
       }
-    })
-    poly.properties[outField] = ss.variance(values)
+    });
+    poly.properties[outField] = ss.variance(values);
   })
 
   return polyFC;
